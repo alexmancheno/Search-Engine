80795ed30ac1ec15e2d671fbb32d7c0532c463a8
alexmancheno <alex.mancheno@gmail.com>
2018-12-02 18:49:23 -0500

    Added Maven project

---
+++ README.md
@@ -0,0 +1,20 @@
+# 
+
+
+## How to run backend
+1. Change directory into root of Maven project
+```
+cd backend/backend
+```
+
+2. Compile the Maven project
+```
+mvn clean package
+```
+
+3. Run
+```
+java -jar target/backend-0.1.jar
+```
+
+## Deploying backend project to server
---
+++ backend/backend/src/main/java/qc/cs355/backend/backend/DatabaseController.java
@@ -0,0 +1,16 @@
+package qc.cs355.backend.backend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class DatabaseController 
+{
+
+    @RequestMapping("/")
+    public String index() 
+    {
+        return "Greetings from Spring Boot!";
+    }
+
+}

ed852d9b057bee2201c9c328bf9dda38ace6fa6b
alexmancheno <alex.mancheno@gmail.com>
2018-12-04 16:34:02 -0500

    Renamed a lot of the folders/files

---
+++ search-engine/application/src/main/java/qc/cs355/application/controllers/DatabaseController.java
@@ -0,0 +1,21 @@
+package qc.cs355.application;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * This is the controller that should get requests that should respond with JSON.
+ * Think of this controller as a RESTful service controller.
+ */
+// @RestController
+public class DatabaseController 
+{
+
+    // @RequestMapping("/")
+    // public String index() 
+    // {
+    //     return "Greetings from Phat Search!!!!";
+    // }
+
+}
---
+++ search-engine/application/src/main/java/qc/cs355/application/controllers/FrontendController.java
@@ -0,0 +1,22 @@
+package qc.cs355.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * This is the controller that should get requests that should respond with
+ * HTML pages to render. 
+ */
+@Controller
+public class FrontendController 
+{
+
+    @GetMapping("/")
+    public String greeting()
+    {
+        System.out.println("hey!!");
+        return "index";
+    }
+}

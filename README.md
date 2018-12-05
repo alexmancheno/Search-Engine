1969a3cff1ba90b310899a523e686afd39cf25f4
alexmancheno <alex.mancheno@gmail.com>
2018-12-04 19:53:02 -0500

    Set up the frontend project so that we can work on it too. Updated README.md with instructions on how to run/bundle frontend.

--- README.md
+++ README.md
@@ -19,6 +19,16 @@ Alternatively, you can run the `mvn exec` plugin (faster):
 mvn compile exec:java
 ```
 
+**Note:** if you don't have Maven installed, use the Maven wrapper instead. Example:
+```
+./mvnw clean package
+```
+
+If you need to grant permissions on Mac for the Maven wrapper:
+```
+chmod a+x mvnw
+```
+
 #### Deploying to server
 1. Compile the Maven project
 ```
@@ -38,4 +48,21 @@ service search-engine-backend restart
 4. Test (while logged into server)
 ```
 curl localhost:8080
+```
+
+### Frontend
+#### How to run
+1. Change into the root directory of the frontend
+```
+cd search-engine/application/src/main/frontend
+```
+
+2. Run and go to localhost:8080 (or 8081):
+```
+npm run start
+```
+
+#### How to output the bundle that Spring Boot backend reads from
+```
+npm run build
 ```
---
+++ search-engine/application/src/main/frontend/webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+// Generates the output
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "index.html"
+});
+
+module.exports = {
+    output: {
+        path: __dirname + "./../resources/templates",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    },
+                    {
+                        loader: "react-svg-loader",
+                        options: {
+                            jsx: true // true outputs JSX tags
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
+};

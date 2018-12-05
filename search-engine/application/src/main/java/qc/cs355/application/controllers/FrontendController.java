e8abe7bc9805bb8e41e281aae9a7b0b14f55d08e
alexmancheno <alex.mancheno@gmail.com>
2018-12-04 21:37:04 -0500

    Made a change to FrontendController.java

--- search-engine/application/src/main/java/qc/cs355/application/controllers/FrontendController.java
+++ search-engine/application/src/main/java/qc/cs355/application/controllers/FrontendController.java
@@ -1,9 +1,7 @@
 package qc.cs355.application.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * This is the controller that should get requests that should respond with
@@ -13,10 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FrontendController 
 {
 
-    @GetMapping("/")
-    public String greeting()
-    {
-        System.out.println("hey!!!!!!!!");
-        return "index";
-    }
+    /**
+     * Actually, this isn't necessary since Spring Boot serves by default index.html
+     * in src/resources/static/, without the need of a controller.
+     */
+
+    // @GetMapping("/")
+    // public String greeting()
+    // {
+    //     System.out.println("hey!!!!!!!!");
+    //     return "index";
+    // }
 }

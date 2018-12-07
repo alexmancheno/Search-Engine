9df72d59f5e54e87d01de46ebb3e79283df32188
alexmancheno <alex.mancheno@gmail.com>
2018-12-07 17:03:31 -0500

    Working on make the frontend and backend be able to communicate

--- search-engine/application/src/main/frontend/src/components/user/SearchBar/SearchBar.js
+++ search-engine/application/src/main/frontend/src/components/user/SearchBar/SearchBar.js
@@ -1,14 +1,34 @@
 import React,  {Component} from 'react';
+import axios from 'axios';
 import style from './style.css';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.runPhatSearch = this.runPhatSearch.bind(this);
+    }
+
+    runPhatSearch(query) {
+        console.log("hey!");
+        axios.get('https://localhost:8080/search', {
+            params: {
+                query: "hello"
+            }
+        }).then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.log(error);
+        })
+         
+    }
+
     render() {
         return (
             <div className={style.main} >
                 <div className="input-group">
                     <input type="text" className="form-control" placeholder="Get a phat search" />
                     <div className="input-group-append">
-                        <button className="btn btn-secondary" type="button">
+                        <button className="btn btn-secondary" type="button" onClick={e => this.runPhatSearch('testing')}>
                             <i className="fa fa-search"></i>
                         </button>
                     </div>
--- search-engine/application/src/main/java/qc/cs355/application/controllers/DatabaseController.java
+++ search-engine/application/src/main/java/qc/cs355/application/controllers/DatabaseController.java
@@ -1,21 +1,32 @@
 package qc.cs355.application.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 /**
  * This is the controller that should get requests that should respond with JSON.
  * Think of this controller as a RESTful service controller.
  */
-// @RestController
+@RestController
 public class DatabaseController 
 {
 
-    // @RequestMapping("/")
-    // public String index() 
-    // {
-    //     return "Greetings from Phat Search!!!!";
-    // }
+    @RequestMapping("/search")
+    public List<String> search(@RequestParam("query") String query) 
+    {
+        System.out.printf("Query: %s\n", query);
+        List<String> results = new ArrayList<>();
+        results.add("result #1");
+        results.add("result #2");
+        results.add("result #3");
+
+        return results;
+    }
 
 }

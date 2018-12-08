7afd4089b466dd45def7ba80b3ef4d2acc44295f
cemfahli <cemfahli@gmail.com>
2018-12-08 13:54:25 -0500

    Inserted capability to start web crawler, commented out

--- search-engine/application/src/main/java/qc/cs355/application/Application.java
+++ search-engine/application/src/main/java/qc/cs355/application/Application.java
@@ -3,12 +3,17 @@ package qc.cs355.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import qc.cs355.application.webcrawler.pWebCrawler;
+
 @SpringBootApplication
 public class Application 
 {
 
 	public static void main(String[] args) 
 	{
+		//Starting the web crawler at server start, with the first site
+		//being cnn, and the maximum number of crawled sites set at 100000
+		//new Thread(new pWebCrawler("https://www.cnn.com", 100000));
 		SpringApplication.run(Application.class, args);
 	}
 }

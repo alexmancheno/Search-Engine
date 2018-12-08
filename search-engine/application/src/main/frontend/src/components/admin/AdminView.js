2850453080ec8b896489551f65508762b6557c4f
alexmancheno <alex.mancheno@gmail.com>
2018-12-08 13:49:29 -0500

    Updated the frontend some more.

---
+++ search-engine/application/src/main/frontend/src/components/admin/AdminView.js
@@ -0,0 +1,15 @@
+import React,  {Component} from 'react';
+
+export default class AdminView extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>hey</h1>
+            </div>
+        )
+    }
+}
--- search-engine/application/src/main/java/qc/cs355/application/controllers/DatabaseController.java
+++ search-engine/application/src/main/java/qc/cs355/application/controllers/DatabaseController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 /**
@@ -18,15 +21,19 @@ public class DatabaseController
 {
 
     @RequestMapping("/search")
-    public List<String> search(@RequestParam("query") String query) 
+    @CrossOrigin(origins = "http://localhost:8081")
+    public String search(@RequestParam("query") String query) 
     {
+
+        // List<String> results = Database.search(query);
         System.out.printf("Query: %s\n", query);
         List<String> results = new ArrayList<>();
         results.add("result #1");
         results.add("result #2");
         results.add("result #3");
 
-        return results;
+        
+        return new Gson().toJson(results);
     }
 
 }
